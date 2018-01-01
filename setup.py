d3e690284cf68a33e3ccb3a90e2a5bf98debfb25
Thomas Prebble <thomas.prebble@gmail.com>
2018-01-02 00:14:50 +0900

    Fixing setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
@@ -20,8 +20,8 @@ setup(
     ],
     tests_require=[
         "Flask-WTF"
-    ]
-    test_suites="tests",
+    ],
+    test_suite="tests",
     classifiers=[
         "Environment :: Web Environment",
         "Intended Audience :: Developers",
